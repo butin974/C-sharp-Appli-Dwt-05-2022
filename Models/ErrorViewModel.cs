d835fa7463b1cf3d852df56e9236cf2aed6e0c7c
butin974 <d.butin@wanadoo.fr>
2022-05-31 06:13:38 +0200

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace moi_Dwt_05_2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
